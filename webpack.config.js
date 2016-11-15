b062ff7c750152e1d138c23fdee0fe196c77a912
JC Balcita <john.balcita@gmail.com>
2016-11-15 14:53:06 -0800

    renders static boxes on canvas

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./lib/iloveblocks.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+};
